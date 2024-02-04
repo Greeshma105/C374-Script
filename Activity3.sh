a47d26f13c0bc320750433204c498be9637aebef
Greeshma105 <greeshmats105@gmail.com>
2024-02-04 23:43:04 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Activity3.sh
    	new file:   lottery numbers 2024-02-04.txt
    	new file:   lottery_numbers_2024-02-04.txt

--- Activity3.sh
+++ Activity3.sh
@@ -1,1 +1,12 @@
-echo Hello
+#!/bin/bash
+
+current_date=$(date +"%Y-%m-%d")
+
+file_name="lottery_numbers_$current_date.txt"
+
+numbers=$(shuf -i 1-50 -n 5) > "$file_name"
+
+bonus_ball=$(shuf -i 1-10 -n 1)
+echo "Bonus Ball: $bonus_ball" >> "$file_name"
+
+echo "Lottery numbers generated and saved to $file_name"
