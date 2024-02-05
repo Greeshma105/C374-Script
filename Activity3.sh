9db3acd32c92df97e70e99f286c580a23a60b44a
Greeshma105 <greeshmats105@gmail.com>
2024-02-05 11:29:58 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Activity3.sh

--- Activity3.sh
+++ Activity3.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+main_numbers=$(shuf -i 1-50 -n 5 | tr '\n' ' ') #shuffle no between 1 to 50, take 5 numbers and turncate new line with sapce
+bonus_ball=$(shuf -i 1-10 -n 1)  #shuffle no between 1 to 10, take 1 numbers
+date_string=$(date +"%Y-%m-%d")
+filename="lottery_numbers_${date_string}.txt"
 
-current_date=$(date +"%Y-%m-%d")
+echo "$main_numbers" > "$filename"
+echo "$bonus_ball" >> "$filename"
 
-file_name="lottery_numbers_$current_date.txt"
+echo "Lottery numbers $main_numbers and Bonus Ball $bonus_ball generated and saved to $filename."
 
-numbers=$(shuf -i 1-50 -n 5) > "$file_name"
-
-bonus_ball=$(shuf -i 1-10 -n 1)
-echo "Bonus Ball: $bonus_ball" >> "$file_name"
-
-echo "Lottery numbers generated and saved to $file_name"
